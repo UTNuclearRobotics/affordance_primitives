bdfeffbbfc6290539b677a891d676604db3ae68c
AdamPettinger <adam.pettinger@utexas.edu>
2022-01-26 15:48:22 -0600

    Add ParameterManager plugin (#7)
    * Add ParameterManager plugin
    
    * Add tests for empty parameter manager

---
+++ affordance_primitives/src/parameter_manager_plugins.cpp
@@ -0,0 +1,5 @@
+#include <pluginlib/class_list_macros.h>
+#include <affordance_primitives/empty_parameter_manager.hpp>
+#include <affordance_primitives/parameter_manager.hpp>
+
+PLUGINLIB_EXPORT_CLASS(affordance_primitives::EmptyParameterManager, affordance_primitives::ParameterManager)
